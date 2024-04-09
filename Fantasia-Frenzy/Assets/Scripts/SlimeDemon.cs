924ce4810faa3ef10cc73010bc2c5f9de3c32735
madsmarzano <139702426+madsmarzano@users.noreply.github.com>
2024-04-09 10:54:53 -0400

    Slime Demon - Anim Script
    Configured slime demon to pop up if player is in range, and retract if player is out of range

--- Fantasia-Frenzy/Assets/Scripts/SlimeDemon.cs
+++ Fantasia-Frenzy/Assets/Scripts/SlimeDemon.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class SlimeDemon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform target;
+    public float attackRange;
+
+    private bool isUp = false;
+
+    private Animator animator;
+
+    private void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Vector2.Distance(transform.position, target.position) < attackRange && !isUp)
+        {
+            PopUp();
+        }
+        else if (Vector2.Distance(transform.position, target.position) > attackRange && isUp)
+        {
+            Retract();
+        }
+    }
+
+    private void PopUp()
+    {
+        animator.Play("Popup");
+        isUp = true;
+    }
+
+    private void Retract()
+    {
+        animator.Play("Retract");
+        isUp = false;
     }
 }
