acde03c7360f758ceb88d69da6dd744543f7983f
madsmarzano <139702426+madsmarzano@users.noreply.github.com>
2024-04-09 19:00:59 -0400

    working on little demon

--- Fantasia-Frenzy/Assets/Scripts/LittleDemon.cs
+++ Fantasia-Frenzy/Assets/Scripts/LittleDemon.cs
@@ -10,6 +10,8 @@ public class LittleDemon : MonoBehaviour
     public Transform target;
     public float attackDistance;
 
+    private bool isHolding = false;
+
     private Vector2 newPos;
 
     private Rigidbody2D rb;
@@ -21,24 +23,47 @@ public class LittleDemon : MonoBehaviour
 
     private void Update()
     {
-        //Vector2 newPos = new Vector2(runDistance, transform.position.y);
-        Vector2 targetPos = new Vector2 (target.position.x, transform.position.y);
+        GetDirection();
+
+        Vector2 targetPos = new Vector2 (target.position.x * runDistance, transform.position.y);
         if (Vector2.Distance(transform.position, target.position) < attackDistance)
         {
-            //transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
-            //transform.position = new Vector2(target.position.x * overshoot, transform.position.y);
-            if (target.position.x < transform.position.x)
-            {
-                //enemy moves left
-                newPos = new Vector2(-runDistance, transform.position.y);
-                transform.position = Vector2.MoveTowards(transform.position, newPos, speed * Time.deltaTime);
-            }
-            else if (target.position.x > transform.position.x)
-            {
-                //enemy moves right
-                newPos = new Vector2(runDistance, transform.position.y);
-                transform.position = Vector2.MoveTowards(transform.position, newPos, speed * Time.deltaTime);
-            }
+            //Charge();
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+            //isHolding = true;
+        }
+    }
+
+    private void GetDirection()
+    {
+        if (target.position.x < transform.position.x)
+        {
+            //enemy moves left
+            runDistance = -2f;
+        }
+        else if (target.position.x > transform.position.x)
+        {
+            //enemy moves right
+            runDistance = 2f;
+        }
+    }
+
+    private void Charge()
+    {
+        //transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        //transform.position = new Vector2(target.position.x * overshoot, transform.position.y);
+        if (target.position.x < transform.position.x)
+        {
+            //enemy moves left
+            newPos = new Vector2(-runDistance, transform.position.y);
+            transform.position = Vector2.MoveTowards(transform.position, newPos, speed * Time.deltaTime);
+        }
+        else if (target.position.x > transform.position.x)
+        {
+            //enemy moves right
+            newPos = new Vector2(runDistance, transform.position.y);
+            transform.position = Vector2.MoveTowards(transform.position, newPos, speed * Time.deltaTime);
         }
     }
 }
