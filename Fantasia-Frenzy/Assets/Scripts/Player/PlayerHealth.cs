05fbe93cf9c3a15a8f082efda81da6586a65f41e
madsmarzano <139702426+madsmarzano@users.noreply.github.com>
2024-04-11 11:06:35 -0400

    Created PlayerHealth script

---
+++ Fantasia-Frenzy/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] private int health;
+    [SerializeField] private int maxHealth;
+
+
+}
