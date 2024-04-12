6ce204be066ca4a576a8f5a787356a91dad1fec5
madsmarzano <139702426+madsmarzano@users.noreply.github.com>
2024-04-12 19:28:04 -0400

    Implement damage effect for Player

--- Fantasia-Frenzy/Assets/Scripts/Player/PlayerHealth.cs
+++ Fantasia-Frenzy/Assets/Scripts/Player/PlayerHealth.cs
@@ -7,5 +7,36 @@ public class PlayerHealth : MonoBehaviour
     public int health;
     [SerializeField] private int maxHealth;
 
+    private SpriteRenderer _spriteRenderer;
+    [SerializeField] private Material flashMaterial;
+    [SerializeField] private Material originalMaterial;
+
+    private Coroutine _damageEffect;
+
+    private void Start()
+    {
+        _spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        originalMaterial = _spriteRenderer.material;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            if (_damageEffect != null)
+            {
+                StopCoroutine(_damageEffect);
+            }
+            _damageEffect = StartCoroutine(DamageEffect());
+        }
+    }
+
+    IEnumerator DamageEffect()
+    {
+        _spriteRenderer.material = flashMaterial;
+        yield return new WaitForSeconds(0.15f);
+        _spriteRenderer.material = originalMaterial;
+        _damageEffect = null;
+    }
 
 }
