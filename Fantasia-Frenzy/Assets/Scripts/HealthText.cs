bc845166f9a9c1a6344ae0153039c3fc153970fc
madsmarzano <139702426+madsmarzano@users.noreply.github.com>
2024-04-13 15:16:03 -0400

    Connected Health to UI

---
+++ Fantasia-Frenzy/Assets/Scripts/HealthText.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class HealthText : MonoBehaviour
+{
+    private TMP_Text healthTxt;
+    [SerializeField] PlayerHealthValue health;
+
+    private void Start()
+    {
+        healthTxt = GetComponent<TMP_Text>();
+    }
+
+    private void Update()
+    {
+        healthTxt.SetText("Health: " + health.value.ToString() + "%");
+    }
+}
