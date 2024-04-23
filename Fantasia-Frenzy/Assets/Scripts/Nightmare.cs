544812eaa05719e733fa75e97d0c745c6fa88ee6
madsmarzano <139702426+madsmarzano@users.noreply.github.com>
2024-04-23 11:06:23 -0400

    Work on Nightmare logic

--- Fantasia-Frenzy/Assets/Scripts/Nightmare.cs
+++ Fantasia-Frenzy/Assets/Scripts/Nightmare.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Nightmare : MonoBehaviour
 {
     public float floatSpeed = 3f;
+    public Transform pointA;
+    public Transform pointB;
+
+    private Vector3 nextPosition;
+
     public bool isDead = false;
     private EnemyHealth enemyHealth;
+
+    private bool isAttacking= false;
+    [SerializeField] private GameObject lightning;
+    public float distance;
 
     Rigidbody2D rb;
 
     private void Start()
     {
         enemyHealth = GetComponent<EnemyHealth>();
+        nextPosition = pointA.position;
     }
 
     private void Awake()
@@ -22,6 +33,31 @@ public class Nightmare : MonoBehaviour
     
     private void Update()
     {
+        {
+            if (!isAttacking)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, nextPosition, floatSpeed * Time.deltaTime);
+            }
+
+            if (transform.position == nextPosition)
+            {
+                nextPosition = (nextPosition == pointA.position) ? pointB.position : pointA.position;
+            }
+        }
+
+        Physics2D.queriesStartInColliders = false;
+        if (!isDead)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, distance);
+            Debug.DrawRay(transform.position, Vector2.down * distance, Color.red);
+
+            if (hit.collider.CompareTag("Player") && !isAttacking)
+            {
+                //isAttacking = true;
+                StartCoroutine(Attack());
+            }
+        }
+
         if (enemyHealth.health == 0)
         {
             isDead = true;
@@ -29,12 +65,13 @@ public class Nightmare : MonoBehaviour
         }
     }
 
-    private void FixedUpdate()
+    IEnumerator Attack()
     {
-        if (!isDead)
-        {
-            //Nightmare moves
-            rb.velocity = new Vector2(floatSpeed * Vector2.left.x, rb.velocity.y);
-        }
+        isAttacking = true;
+        GameObject lightningInst = Instantiate(lightning, transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(2f);
+        isAttacking = false;
     }
+
+
 }
