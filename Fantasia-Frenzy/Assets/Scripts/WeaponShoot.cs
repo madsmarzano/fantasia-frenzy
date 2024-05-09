b09a731290b94b578419d109e9956f03acab5218
madsmarzano <139702426+madsmarzano@users.noreply.github.com>
2024-05-08 22:28:34 -0400

    bee updates and weapon updates
    weapon variations

--- Fantasia-Frenzy/Assets/Scripts/WeaponShoot.cs
+++ Fantasia-Frenzy/Assets/Scripts/WeaponShoot.cs
@@ -10,22 +10,66 @@ public class WeaponShoot : MonoBehaviour
     [SerializeField] GameObject bolt;
     [SerializeField] GameObject bulletSpawnPoint;
 
+    [SerializeField] float waitTime = 0;
+    [SerializeField] float reloadTime;
+    [SerializeField] float magCount;
+    private float fullMag;
+
+    private bool isShooting = false;
+    private bool isResetting = false;
+
     //private bool boltActive = false;
+
+    private void Start()
+    {
+        fullMag = magCount;
+    }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0) && this.CompareTag("Projectile"))
-            Shoot();
+        if (Input.GetMouseButtonDown(0) && this.CompareTag("Projectile") && !isShooting)
+            StartCoroutine(Shoot());
+
+        if (Input.GetMouseButton(0) && this.CompareTag("FullAuto") && !isShooting && magCount > 0)
+            StartCoroutine(FullAuto());
 
         if (Input.GetMouseButtonDown(0) && this.CompareTag("Bolt"))
             Bolt();
+
+        if (magCount == 0 && !isResetting)
+        {
+            StartCoroutine(ResetMag());
+        }
     }
 
-    private void Shoot()
+    IEnumerator Shoot()
     {
         //spawn bullet
+        isShooting = true;
         GameObject bulletInst = Instantiate(bullet, bulletSpawnPoint.transform.position, this.transform.rotation);
         animator.Play("Shoot", 0);
+        yield return new WaitForSeconds(waitTime);
+        isShooting = false;
+
+    }
+
+    IEnumerator FullAuto()
+    {
+        //spawn bullet
+        isShooting = true;
+        GameObject bulletInst = Instantiate(bullet, bulletSpawnPoint.transform.position, this.transform.rotation);
+        animator.Play("Shoot", 0);
+        magCount--;
+        yield return new WaitForSeconds(waitTime);
+        isShooting = false;
+    }
+
+    IEnumerator ResetMag()
+    {
+        isResetting = true;
+        yield return new WaitForSeconds(reloadTime);
+        magCount = fullMag;
+        isResetting = false;
 
     }
 
